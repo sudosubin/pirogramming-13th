070c70bc817d559a9757b7eacaadfeb58773139a
sudosubin <sudosubin+github@gmail.com>
2020-07-26 13:57:03 +0900

    Change update url configuration

--- diary/article/urls.py
+++ diary/article/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', index, name='list'),
     path('create/', create, name='create'),
     path('article/<int:pk>/', retrieve, name='retrieve'),
-    path('update/<int:pk>/', update, name='update'),
+    path('article/<int:pk>/update/', update, name='update'),
     path('article/<int:pk>/delete/', delete, name='delete'),
 ]
